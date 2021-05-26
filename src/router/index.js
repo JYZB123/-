a0d081a2e7724fe500d3f73a86bc4bc5a01228e0
JYZB123 <2757528582@qq.com>
2021-05-26 09:52:40 +0800

    vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,63 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Student from '../views/Student'
+import Teacher from '../views/Teacher'
+import Home from '../views/Home'
+import Login from '../views/Login'
+import Course from "../views/Course";
+import CourseHome from "../views/CourseHome";
+import Course_Introduction from "../views/Course_Introduction";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
+
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/introduction',
+    name: 'Course_Introduction',
+    component: Course_Introduction,
+    hidden: true,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/courseHome',
+    name: 'CourseHome',
+    component: CourseHome,
+    hidden: true,
+  },
+  {
+    path: '/',
+    name: 'Login',
+    component: Login,
+    hidden: true
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    hidden: true
+  },
+  {
+    path: '/home',
+    name: '导航一',
+    component: Home,
+    children:[
+      {
+        path: '/student',
+        name: '管理学生',
+        component: Student
+      },
+      {
+        path: '/teacher',
+        name: '管理教师',
+        component: Teacher
+      },
+      {
+        path: '/course',
+        name: '管理课程',
+        component: Course
+      }
+    ]
+  },
 ]
 
 const router = new VueRouter({
